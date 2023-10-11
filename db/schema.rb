c3fda0598d40db534e55692e0e75a196910f5178
Yasirhussain143 <yasir.lqp@gmail.com>
2023-10-12 02:16:44 +0500

    add a teacher table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_210356) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_205639) do
   create_table "students", force: :cascade do |t|
     t.integer "student_id"
     t.string "student_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
